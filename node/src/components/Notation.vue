0a46c1bf82b00e1311e18753dc5475521b63c6be
David <db@Davids-Air.lan1>
2020-04-10 03:46:17 -0700

    play music

--- node/src/components/Notation.vue
+++ node/src/components/Notation.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+<div>
+  
     {{ sounds }}
-    
-<div id="osmdCanvas">
+    <div>
+      <a href="#" @click="play" style="font-size:2em; color:black" v-html="playButton"></a>
+    </div>
+    <div id="osmdCanvas">
 </div>
 </div>
 </template>
@@ -10,13 +13,30 @@
 <script>
 import axios from 'axios'
 import { handleFileSelect } from '@/../public/js/osmd/xml-loader.js'
-import { playScale } from '@/../public/js/wad/playscale.js'
+import * as  Wad from 'web-audio-daw';
+
+function togglePlayButton(btn) {
+    return (btn == "&#9656; (play)") ?  "&#9646; &#9646; (pause)" :  "&#9656; (play)";
+}
+
+function toggleBool(tf) {
+    return tf ? false : true;
+}
+
+
+// var stopped = true;
+var currentNote = 0
+var bpm = 60; // document.getElementById('bpmbox').value;
+var pause = 60000 / bpm;
+var sustain = pause / 2000;
 
 export default ({
 data () {
 return {
 notation: null,
-sounds: null
+sounds: null,
+playButton: "&#9656; (play)",
+tf: false
 }
 },
 
@@ -39,13 +59,35 @@ this.$nextTick(); // wait for re-render
 handleFileSelect(this.notation);
 
 this.sounds = JSON.parse(result.data.sound.replace(/'/g, '"'));
-playScale(this.sounds);
 
+},
 
+methods: {
+play: function() {
+this.tf = toggleBool(this.tf);
+this.playButton = togglePlayButton(this.playButton);
+this.playscale();
+},
 
+playscale: function () {
+    if(this.tf){
+        var piano = new Wad(Wad.presets.piano);
+        piano.play({pitch: this.sounds[currentNote],
+                    env: {hold: sustain}});
 
-console.log(this.sounds);
-console.log(typeof this.sounds);
+   currentNote++;
+   currentNote%=this.sounds.length;
+
+   if(currentNote == 0){
+       pause = 4000;
+   }
+        else {
+            pause = 60000 / bpm;
+        }
+
+        setTimeout(this.playscale, pause);        
+     }
+}
 
 }})
 
