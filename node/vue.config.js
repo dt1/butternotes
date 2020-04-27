cbc3a4adeb2c530cd9566582ccb66dccd39ca84b
dt1 <dbtoomey@gmail.com>
2020-04-26 21:58:59 -0700

    package updates

--- node/vue.config.js
+++ node/vue.config.js
@@ -4,6 +4,15 @@ module.exports = {
     runtimeCompiler: true,
     devServer: {
         proxy: 'https://127.0.0.1:3000/',
-    }
-
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                options.prettify = false
+                return options
+        })
+  }
 }
